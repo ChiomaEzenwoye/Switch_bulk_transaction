e697ffa9f1a323a5ef2b1f53fead94ff832639d3
CHIOMA EZENWOYE <admin@Chiomas-MacBook-Air.local>
2025-11-01 09:04:50 +0100

    impl for bulk service

--- src/main/java/com/example/switchbulktransaction/controller/BulkTransactionController.java
+++ src/main/java/com/example/switchbulktransaction/controller/BulkTransactionController.java
@@ -1,34 +1,30 @@
 package com.example.switchbulktransaction.controller;
 
 import com.example.switchbulktransaction.model.dto.request.BulkTransactionRequest;
-import com.example.switchbulktransaction.model.dto.response.BulkTransactionResponse;
+import com.example.switchbulktransaction.model.dto.response.ApiResponse;
+import com.example.switchbulktransaction.service.BulkProcessingService;
 import com.example.switchbulktransaction.util.Roles;
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/")
+@RequiredArgsConstructor
 public class BulkTransactionController {
 
+    private final BulkProcessingService bulkTransactionService;
 
     @PreAuthorize(Roles.HAS_USER_ROLE)
     @PostMapping("bulk-transactions")
-    public BulkTransactionResponse processBulkTransactions(@Valid @RequestBody BulkTransactionRequest request){
-        return new BulkTransactionResponse();
+    public ApiResponse<?> processBulkTransactions(@Valid @RequestBody BulkTransactionRequest request) {
+        return bulkTransactionService.processBulkTransactions(request);
     }
 
     @PreAuthorize(Roles.HAS_ADMIN_ROLE)
     @PostMapping("bulk-transactions-admin")
-    public BulkTransactionResponse testAdmin(@Valid @RequestBody BulkTransactionRequest request){
-        return new BulkTransactionResponse();
+    public ApiResponse<?> testAdmin(@Valid @RequestBody BulkTransactionRequest request) {
+        return bulkTransactionService.processBulkTransactions(request);
     }
-
-
-
 }
